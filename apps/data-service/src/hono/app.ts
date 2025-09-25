847911e30afb91efdcd7ea86af19fb59d16e5b46
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-25 22:24:38 +0530

    Get location info from CF specific request headers.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -3,7 +3,16 @@ import { Hono } from 'hono';
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
+	// Print CF specific info from the request
+	console.log(JSON.stringify(c.req.raw.cf));
+	// c.req.raw is the original forwarded Request object from the CF worker
+	const cf = c.req.raw.cf;
+	const country = cf?.country;
+	const lat = cf?.latitude;
+	const long = cf?.longitude;
 	return c.json({
-		message: 'Hello World!',
+		country,
+		lat,
+		long,
 	});
 });
--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -3,6 +3,7 @@ import { App } from './hono/app';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
+		// Forward the request to the Hono app
 		return App.fetch(request, this.env, this.ctx);
 	}
 }
