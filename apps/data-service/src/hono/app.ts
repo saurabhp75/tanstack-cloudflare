40f053055733111baa78d66b3564179b0db10417
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-26 17:09:41 +0530

    Add the link click data to the CF queue for further processing.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,5 +1,6 @@
 import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
@@ -20,5 +21,26 @@ App.get('/:id', async (c) => {
 	const headers = cfHeader.data;
 	console.log(headers);
 	const destination = getDestinationForCountry(linkInfo, headers.country);
+
+	const queueMessage: LinkClickMessageType = {
+		type: 'LINK_CLICK',
+		data: {
+			id: id,
+			country: headers.country,
+			destination: destination,
+			accountId: linkInfo.accountId,
+			latitude: headers.latitude,
+			longitude: headers.longitude,
+			timestamp: new Date().toISOString(),
+		},
+	};
+
+	// Ask CF worker to close only after sending the message to queue.
+	// The execution won't stop here like 'await' and the next line
+	// will be executed immediately. Very rarely the worker might close
+	// down before the message is sent to queue. So, this is not used for
+	// financial or critical data.
+	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
+
 	return c.redirect(destination);
 });
