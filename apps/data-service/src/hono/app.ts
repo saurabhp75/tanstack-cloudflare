859e57c93066f4ad893d590aea0c94336dc039f6
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-26 14:11:04 +0530

    Connect data-service with the user application db.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,18 +1,12 @@
+import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-	// Print CF specific info from the request
-	console.log(JSON.stringify(c.req.raw.cf));
-	// c.req.raw is the original forwarded Request object from the CF worker
-	const cf = c.req.raw.cf;
-	const country = cf?.country;
-	const lat = cf?.latitude;
-	const long = cf?.longitude;
-	return c.json({
-		country,
-		lat,
-		long,
-	});
+	
+	const id = c.req.param('id');
+	const linkInfoFromDb = await getLink(id);
+
+	return c.json(linkInfoFromDb);
 });
--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,7 +1,13 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { App } from './hono/app';
+import { initDatabase } from '@repo/data-ops/database';
 
 export default class DataService extends WorkerEntrypoint<Env> {
+	constructor(ctx: ExecutionContext, env: Env) {
+		super(ctx, env);
+		initDatabase(env.DB);
+	}
+
 	fetch(request: Request) {
 		// Forward the request to the Hono app
 		return App.fetch(request, this.env, this.ctx);
