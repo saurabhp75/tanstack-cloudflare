d345cec3b0c5e218f451b25dcc3fd81c680d67c3
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-27 19:14:49 +0530

    Scrape the contents of the url in workflow using CF puppeteer.

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,12 +1,10 @@
+import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-	async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
 		const collectedData = await step.do('Collect rendered destination page data', async () => {
-			console.log('Collecting rendered destination page data');
-			return {
-				dummydata: 'dummydata',
-			};
+			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
 		console.log(collectedData);
 	}
