d2ed85b14fd86b92c7b98f55dbd4a222fe285d5a
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-28 15:03:19 +0530

    Add a workflow step to analyse page contents using AI.

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,3 +1,4 @@
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
@@ -6,6 +7,21 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
 		const collectedData = await step.do('Collect rendered destination page data', async () => {
 			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
+
+		const aiStatus = await step.do(
+			'Use AI to check status of page',
+			{
+				// Don't retry as AI is expnensive
+				retries: {
+					limit: 0,
+					delay: 0,
+				},
+			},
+			async () => {
+				return await aiDestinationChecker(this.env, collectedData.bodyText);
+			}
+		);
+
 		console.log(collectedData);
 	}
 }
