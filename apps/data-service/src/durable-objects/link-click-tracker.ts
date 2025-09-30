bb9c9a8d43747b59b21401663b97c9d3f0841d98
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-30 21:43:50 +0530

    Send link clicks to all connected clients via DO websocket.

--- apps/data-service/src/durable-objects/link-click-tracker.ts
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -1,13 +1,24 @@
+import { deleteClicksBefore, getRecentClicks } from '@/helpers/durable-queries';
 import { DurableObject } from 'cloudflare:workers';
+import moment from 'moment';
 
 export class LinkClickTracker extends DurableObject {
 	sql: SqlStorage;
+	mostRecentOffsetTime: number = 0;
+	leastRecentOffsetTime: number = 0;
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		this.sql = ctx.storage.sql;
 
 		ctx.blockConcurrencyWhile(async () => {
+			const [leastRecentOffsetTime, mostRecentOffsetTime] = await Promise.all([
+				ctx.storage.get<number>('leastRecentOffsetTime'),
+				ctx.storage.get<number>('mostRecentOffsetTime'),
+			]);
+
+			this.leastRecentOffsetTime = leastRecentOffsetTime || this.leastRecentOffsetTime;
+			this.mostRecentOffsetTime = mostRecentOffsetTime || this.mostRecentOffsetTime;
 			this.sql.exec(`
             CREATE TABLE IF NOT EXISTS geo_link_clicks (
                 latitude REAL NOT NULL,
@@ -30,6 +41,30 @@ export class LinkClickTracker extends DurableObject {
 			country,
 			time
 		);
+		// Don't send the link-click data immediately, instead wait for the alarm to go off
+		// This way we can batch up multiple clicks into a single message
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm) await this.ctx.storage.setAlarm(moment().add(2, 'seconds').valueOf());
+	}
+
+	async alarm() {
+		// console.log('alarm');
+		const clickData = getRecentClicks(this.sql, this.mostRecentOffsetTime);
+
+		const sockets = this.ctx.getWebSockets();
+		for (const socket of sockets) {
+			socket.send(JSON.stringify(clickData.clicks));
+		}
+
+		await this.flushOffsetTimes(clickData.mostRecentTime, clickData.oldestTime);
+		await deleteClicksBefore(this.sql, clickData.oldestTime);
+	}
+
+	async flushOffsetTimes(mostRecentOffsetTime: number, leastRecentOffsetTime: number) {
+		this.mostRecentOffsetTime = mostRecentOffsetTime;
+		this.leastRecentOffsetTime = leastRecentOffsetTime;
+		await this.ctx.storage.put('mostRecentOffsetTime', this.mostRecentOffsetTime);
+		await this.ctx.storage.put('leastRecentOffsetTime', this.leastRecentOffsetTime);
 	}
 
 	async fetch(_: Request) {
@@ -40,5 +75,9 @@ export class LinkClickTracker extends DurableObject {
 			status: 101,
 			webSocket: client,
 		});
+	}
+
+	webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean): void | Promise<void> {
+		console.log('client closed');
 	}
 }
