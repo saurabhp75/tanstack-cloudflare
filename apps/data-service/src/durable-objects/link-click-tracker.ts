3a76ea03a66a589422abe452f16167048a5f84a7
Saurabh Prakash <saurabhp75@gmail.com>
2025-09-30 20:34:36 +0530

    Add WebSocket support to the DO.

--- apps/data-service/src/durable-objects/link-click-tracker.ts
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -1,5 +1,4 @@
 import { DurableObject } from 'cloudflare:workers';
-// import moment from 'moment';
 
 export class LinkClickTracker extends DurableObject {
 	sql: SqlStorage;
@@ -31,5 +30,15 @@ export class LinkClickTracker extends DurableObject {
 			country,
 			time
 		);
+	}
+
+	async fetch(_: Request) {
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
 	}
 }
--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -44,3 +44,16 @@ App.get('/:id', async (c) => {
 
 	return c.redirect(destination);
 });
+
+App.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const accountId = c.req.header('account-id');
+	if (!accountId) return c.text('No Headers', 404);
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
+});
